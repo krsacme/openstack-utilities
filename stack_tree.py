e19d898b541cdaf904f936f5fd51c6e00f6f6400
Saravanan KR <skramaja@redhat.com>
2017-03-07 10:25:35 +0530

    List includes stack and resource tree with type
    Additionally, if a software config is associated to the stack, the
    list will also have the software config details

--- stack_tree.py
+++ stack_tree.py
@@ -4,22 +4,24 @@ import logging
 import os
 import subprocess
 import sys
+import yaml
 
+from heatclient.common import utils
 from heatclient.v1 import client as heatclient
 from keystoneauth1.identity import v2
 from keystoneauth1 import session
 from keystoneclient.v2_0 import client
 
-logging.basicConfig(level=logging.WARN)
-
 log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 
 ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.INFO)
 formatter = logging.Formatter('%(message)s')
 ch.setFormatter(formatter)
 log.addHandler(ch)
+
+_HEAT_SOFTWARE_CONFIG_TYPE = 'OS::Heat::SoftwareConfig'
 
 def get_orchestration_client():
   OS_AUTH_URL = os.environ['OS_AUTH_URL']
@@ -34,21 +36,59 @@ def get_orchestration_client():
   keystone = client.Client(session=sess)
 
   token = auth.get_token(session=sess)
-  log.info("KRS: token: %s" % token)
+  #log.info("KRS: token: %s" % token)
 
   endpoint = auth.get_endpoint(sess,
-                             service_name='heat', service_type='orchestration')
-  log.info("KRS: endpoint: %s" % endpoint)
+                               service_name='heat',
+                               service_type='orchestration')
+  #log.info("KRS: endpoint: %s" % endpoint)
   return heatclient.Client(
       endpoint=endpoint,
       token=token,
   )
 
+def traverse(hc, stack_id, sc_list):
+  global _HEAT_SOFTWARE_CONFIG_TYPE
+  res_map = {}
+
+  stack_id = stack_id.split('/')[0]
+
+  resources = hc.resources.list(stack_id)
+  for resource in resources:
+    if 'resources' not in res_map:
+      res_map['resources'] = {}
+
+    log.debug("resource: type: %s" % resource.resource_type)
+
+    res_info = {}
+    nested_id = utils.resource_nested_identifier(resource)
+    if nested_id:
+      nested_id = nested_id.split('/')[0]
+      nested_res_list = traverse(hc, nested_id, sc_list)
+      res_info.update({'stacks': {nested_id: nested_res_list}})
+
+    res_info.update({'type': resource.resource_type})
+    if resource.resource_type == _HEAT_SOFTWARE_CONFIG_TYPE:
+      for sc in sc_list:
+        if resource.resource_name in sc.name:
+          res_info.update({'software_config': sc.name})
+          res_info.update({'group': sc.group})
+          break
+
+    res_map['resources'].update({resource.resource_name: res_info})
+
+  return res_map
+
 def main():
-  log.info("KRS: Testing")
   hc = get_orchestration_client()
-  stacks = hc.stacks.get('overcloud')
-  log.info(stacks)
+  stack_id = 'overcloud'
+  stack = hc.stacks.get(stack_id)
+  # software config list
+  sc_list = hc.software_configs.list()
+  log.info(stack.identifier)
+  stack_tree = {}
+  stack_tree.update(traverse(hc, stack_id, sc_list))
+  log.info(yaml.safe_dump(stack_tree, encoding='utf-8', allow_unicode=True, default_flow_style=False))
 
 
 if __name__ == "__main__":
